09ec35ed79e6123249e88babef6486760b929a46
Shrikrishna <Shrikrishna@LAPTOP-6GC5M6NR>
2023-08-04 21:48:40 +0530

    Demo 3 class

---
+++ Demo1Project/src/com/demo1package/Demo3Class.java
@@ -0,0 +1,10 @@
+package com.demo1package;
+
+public class Demo3Class {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Demo 3 class");
+	}
+
+}
