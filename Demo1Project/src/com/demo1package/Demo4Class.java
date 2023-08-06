6370d3c6dbfaef223e914db055f8603a7b592eea
Shrikrishna <Shrikrishna@LAPTOP-6GC5M6NR>
2023-08-06 15:41:12 +0530

    Demo 4

---
+++ Demo1Project/src/com/demo1package/Demo4Class.java
@@ -0,0 +1,11 @@
+package com.demo1package;
+
+public class Demo4Class {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Demo 4 class");
+	}
+
+}
